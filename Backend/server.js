e254363412410a9ca869276190026bab635827f9
Rajan Macaraig <rajanmolemacaraig28@email.com>
2026-03-01 19:24:44 +0800

    Resolved missing node environment

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Static Folder for uploads
-app.use('/uplaods', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //Routes
 app.use('/api/auth', authRoutes);
