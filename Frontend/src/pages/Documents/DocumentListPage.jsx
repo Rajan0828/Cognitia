4768c15caa762ff495bda5b0a65f634ce995ea0e
Rajan Macaraig <rajanmolemacaraig28@email.com>
2026-04-04 13:57:19 +0800

    Feat: Document List Page Initial

--- Frontend/src/pages/Documents/DocumentListPage.jsx
+++ Frontend/src/pages/Documents/DocumentListPage.jsx
@@ -1,5 +1,128 @@
+import { useState, useEffect } from 'react';
+import { Plus, Upload, Trash2, FileText, X } from 'lucide-react';
+import toast from 'react-hot-toast';
+
+import documentService from '../../services/documentService';
+import Spinner from '../../components/Common/Spinner';
+import Button from '../../components/Common/Button';
+
 const DocumentListPage = () => {
-  return <div>DocumentListPage</div>;
+  const [documents, setDocuments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // State for upload modal
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
+  const [uploadFile, setUploadFile] = useState(null);
+  const [uploadTitle, setUploadTitle] = useState('');
+  const [upoading, setUploading] = useState(false);
+
+  // State for delete confirmation modal
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+  const [selectDocument, setSelectDocument] = useState(null);
+
+  const fetchDocuments = async () => {
+    try {
+      const data = await documentService.getDocuments();
+      setDocuments(data);
+    } catch (error) {
+      toast.error('Failed to load documents');
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDocuments();
+  }, []);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setUploadFile(file);
+      setUploadTitle(file.name.replace(/\.[^/.]+$/, '')); // Remove file extension for title
+    }
+  };
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    if (!uploadFile || !uploadTitle) {
+      toast.error('Please select a file and enter a title');
+      return;
+    }
+    setUploading(true);
+    const formData = new FormData();
+    formData.append('file', uploadFile);
+    formData.append('title', uploadTitle);
+
+    try {
+      await documentService.uploadDocument(formData);
+      toast.success('Document uploaded successfully');
+      setIsUploadModalOpen(false);
+      setUploadFile(null);
+      setUploadTitle('');
+      setLoading(true);
+      fetchDocuments();
+    } catch (error) {
+      toast.error(error.message || 'Failed to upload document');
+      console.error(error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const handleDeleteRequest = (document) => {
+    setSelectDocument(document);
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (!selectDocument) return;
+    setDeleting(true);
+    try {
+      await documentService.deleteDocument(selectDocument._id);
+      toast.success(`Document ${selectDocument.title} deleted successfully`);
+      setIsDeleteModalOpen(false);
+      setSelectDocument(null);
+      setDocuments(documents.filter((doc) => doc._id !== selectDocument._id));
+    } catch (error) {
+      toast.error(error.message || 'Failed to delete document');
+      console.error(error);
+    } finally {
+      setDeleting(false);
+    }
+  };
+
+  const renderContent = () => {
+    return <div>renderContent </div>;
+  };
+
+  return (
+    <div className='min-h-screen'>
+      {/* SUBTLE BACKGROUND PATTERN */}
+      <div className='absolute inset-0 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-size-[16px_16px] opacity-30 pointer-events-none' />
+      <div className='relative max-w-7xl mx-auto'>
+        {/* HEADER */}
+        <div className='flex items-center justify-between mb-10'>
+          <div>
+            <h1 className='text-2xl font-medium text-slate-900 tracking-tight mb-2'>My Documents</h1>
+            <p className=''>Manage your uploaded documents here</p>
+          </div>
+          {documents.length > 0 && (
+            <Button onClick={() => setIsUploadModalOpen(true)}>
+              <Plus
+                className='text-slate-500 text-sm'
+                strokeWidth={2.5}
+              />
+              Upload Document
+            </Button>
+          )}
+        </div>
+        {renderContent()}
+      </div>
+    </div>
+  );
 };
 
 export default DocumentListPage;
